2b7c656b5a50c8b0a8f7036a367e95797e664f2c
Valstion <lordvalstion@gmail.com>
2023-05-23 13:12:08 +0300

    if else if

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 
         int max = 100;
-        int min = 10;
+        int min = 100;
 
 
         if (min == max) {
@@ -19,6 +19,48 @@ public class Main {
         } else {
             System.out.println("min < max");
         }
+        boolean isRaining = true;
+        boolean isSunny = true;
+        boolean isWarm = true;
+
+        if (isRaining == true) {
+            System.out.println("Laukia lyja");
+        } else if (isSunny == true) {
+            System.out.println("sauleta");
+        } else if (isWarm == true) {
+            System.out.println("sauleta is silta");
+        }
+        if ((isSunny || isRaining) && isWarm) {
+            System.out.println("isjungiam sildyma");
+        }
+
+
+        String name;
+        String surname;
+        int year = 1900;
+        int thisYear = 2023;
+        name = "jevgenij";
+        surname = "Diomin";
+        System.out.println("Aš esu " + name + " " + surname + " Man yra " + (thisYear - year) + " metai(ų)");
+        System.out.println("--------------------------------------------------------");
+        int one = (int) Math.round(Math.random() * 4);
+        System.out.println(one);
+        int two = (int) Math.round(Math.random() * 4);
+        System.out.println(two);
+        if (one == 0 || two == 0){
+            System.out.println("sugenerotas skaicius 0");
+        }
+
+       else
+
+        if (one < two) {
+            System.out.println(two / one);
+        } else if (one > two) {
+            System.out.println(one / two);
+        }
+        else {System.out.println("Skaicia Lygus ");}
+        System.out.println("--------------------------------------------------------");
+
     }
 }
 
---
+++ src/message.java
@@ -0,0 +1,20 @@
+public class message {
+    public static void message() {
+        String name;
+        String surname;
+        int year = 1900;
+        int thisYear = 2023;
+        name = "jevgenij";
+        surname = "Diomin";
+        System.out.println("Aš esu " + name +  surname + " Man yra " + (thisYear - year) +  "metai(ų)");
+
+
+
+
+    }
+    
+
+    
+    
+    
+}
